8be22c6f13868ecdb1eb137c9658c3e545232066
Nestor <nhidlei2602@g.educaand.es>
2024-11-26 13:56:56 +0100

    iniciar sesion

--- ProyectoMyGames/src/main/java/aplication/MainApp.java
+++ ProyectoMyGames/src/main/java/aplication/MainApp.java
@@ -1,145 +1,60 @@
 package aplication;
 
 import java.io.IOException;
-
-import Controllers.PersonEditDialogController;
-import Controllers.PersonOverviewController;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import model.Person;
 
 public class MainApp extends Application {
 
-	private Stage primaryStage;
-	private BorderPane rootLayout;
-	private ObservableList<Person> personData = FXCollections.observableArrayList();
+    private Stage primaryStage;
 
-	/**
-	 * Constructor
-	 */
-	public MainApp() {
-		// Add some sample data
-		Person person1 = new Person("Sofía", "Ramírez", "sofia.ramirez@example.com", "sofiapass", "sofi123");
-		Person person2 = new Person("Miguel", "García", "miguel.garcia@example.com", "miguelpass", "miguelg");
-		Person person3 = new Person("Laura", "López", "laura.lopez@example.com", "laurapass", "laural");
-	}
-  
-	/**
-	 * Returns the data as an observable list of Persons. 
-	 * @return
-	 */
-	public ObservableList<Person> getPersonData() {
-		return personData;
-	}
-	
-	@Override
-	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("AddressApp");
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Initialize if needed
+    }
 
-		initRootLayout();
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Login");
 
-		showPersonOverview();
-	}
-	
-	
+        iniciarSesion();
+    }
 
-	/**
-	 * Initializes the root layout.
-	 */
-	public void initRootLayout() {
-		try {
-			// Load root layout from fxml file.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-			rootLayout = (BorderPane) loader.load();
+    /**
+     * Displays the login window.
+     */
+    public void iniciarSesion() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/views/IniciarSesion.fxml"));
+            AnchorPane iniciarSesion = (AnchorPane) loader.load();
 
-			// Show the scene containing the root layout.
-			Scene scene = new Scene(rootLayout);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            Scene scene = new Scene(iniciarSesion);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error al cargar el archivo FXML: " + e.getMessage());
+        }
+    }
 
-	/**
-	 * Shows the person overview inside the root layout.
-	 */
-	public void showPersonOverview() {
-		try {
-	        // Load person overview.
-	        FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-	        AnchorPane personOverview = (AnchorPane) loader.load();
 
-	        // Set person overview into the center of root layout.
-	        rootLayout.setCenter(personOverview);
+    /**
+     * Returns the main stage.
+     * 
+     * @return the primary stage
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
-	        // Give the controller access to the main app.
-	        PersonOverviewController controller = loader.getController();
-	        controller.setMainApp(this);
-
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
-	
-	/**
-	 * Opens a dialog to edit details for the specified person. If the user
-	 * clicks OK, the changes are saved into the provided person object and true
-	 * is returned.
-	 * 
-	 * @param person the person object to be edited
-	 * @return true if the user clicked OK, false otherwise.
-	 */
-	public boolean showPersonEditDialog(Person person) {
-	    try {
-	        // Load the fxml file and create a new stage for the popup dialog.
-	        FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-	        AnchorPane page = (AnchorPane) loader.load();
-
-	        // Create the dialog Stage.
-	        Stage dialogStage = new Stage();
-	        dialogStage.setTitle("Edit Person");
-	        dialogStage.initModality(Modality.WINDOW_MODAL);
-	        dialogStage.initOwner(primaryStage);
-	        Scene scene = new Scene(page);
-	        dialogStage.setScene(scene);
-
-	        // Set the person into the controller.
-	        PersonEditDialogController controller = loader.getController();
-	        controller.setDialogStage(dialogStage);
-	        controller.setPerson(person);
-
-	        // Show the dialog and wait until the user closes it
-	        dialogStage.showAndWait();
-
-	        return controller.isOkClicked();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return false;
-	    }
-	}
-
-	/**
-	 * Returns the main stage.
-	 * 
-	 * @return
-	 */
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
